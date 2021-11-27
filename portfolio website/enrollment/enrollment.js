6aee84bc9eb082151beb53c3369c157064a0ca8c
rafael15-code <83995087+rafael15-code@users.noreply.github.com>
2021-11-26 23:18:51 -0800

    Add files via upload

---
+++ portfolio website/enrollment/enrollment.js
@@ -0,0 +1,37 @@
+function required()
+{
+var info = document.form1.textbox1.value;
+var info = document.form1.textbox2.value;
+var info = document.form1.textbox3.value;
+var info = document.form1.textbox4.value;
+var info = document.form1.textbox5.value;
+var info = document.form1.textbox6.value;
+var info = document.form1.textbox7.value;
+var info = document.form1.textbox8.value;
+var info = document.form1.textbox9.value;
+var info = document.form1.textbox10.value;
+var info = document.form1.textbox11.value;
+var info = document.form1.textbox12.value;
+var info = document.form1.textbox13.value;
+var info = document.form1.textbox14.value;
+var info = document.form1.textbox15.value;
+var info = document.form1.textbox16.value;
+var info = document.form1.textbox17.value;
+var info = document.form1.textbox18.value;
+var info = document.form1.textbox19.value;
+var info = document.form1.textbox20.value;
+var info = document.form1.textbox21.value;
+var info = document.form1.textbox22.value;
+var info = document.form1.textbox23.value;
+
+if (info === "")
+{
+alert("One or more has no value!");
+return false;
+}
+else 
+{
+alert("Enrollment Form Submitted!");
+return true; 
+}
+}
